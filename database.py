20e43ed5b256057e95669b273b7b933a0c996835
Miles <miles@Miless-MacBook-Pro.local>
2014-11-03 23:25:22 -0600

    Added ability to fetch crimes from the database by id. This will allow us to modify criems to include geolocation data

--- database.py
+++ database.py
@@ -5,6 +5,7 @@ class Database:
     def __init__(self, dbFileLoc):
         self.identity = 0
         self.keys = {'id':0}
+        self.reverseDict{0:'id'}
         self.table = []
         self.dbFileLoc = dbFileLoc
 
@@ -15,6 +16,7 @@ class Database:
                 colRow = next(reader)
                 for cell in colRow:
                     self.keys[cell] = self.col
+                    self.reverseDict[self.col] = key
                     self.col += 1
                 self.table.append(colRow)
                 for row in reader:
@@ -30,6 +32,7 @@ class Database:
         for key in keys:
             if key not in self.keys:
                 self.keys[key] = self.col
+                self.reverseDict[self.col] = key
                 self.col += 1
         for i in range(0, len(self.keys)):
             rowToAdd.append('')
@@ -39,6 +42,20 @@ class Database:
             rowToAdd[self.keys[key]] = crime[key]
 
         self.table.append(rowToAdd)
+
+    def __len__(self):
+        """
+        Returns the number of entries we have
+        """
+        return self.identity + 1
+
+    def getID(self, id):
+        row = self.table[id + 1]
+        dictToReturn = {}
+        for colNum in range(len(row)):
+            dictToReturn[self.reverseDict[colNum]] = row[colNum]
+        return dictToReturn
+            
 
     def close(self):
         newColRow = []
