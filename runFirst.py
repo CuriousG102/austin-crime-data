545f46a132416164660bfa0c3208197990a87230
Miles <miles@Miless-MacBook-Pro.local>
2014-11-15 00:15:29 -0600

    Added geocoding to the setup procedure.

--- runFirst.py
+++ runFirst.py
@@ -1,6 +1,7 @@
 """ Run when you first download this package and it will download and process all the files you need """
 import download
 import process
+import geocode
 
 def main():
     setup()
@@ -8,5 +9,6 @@ def main():
 def setup():
     download.main()
     process.main() 
+    geocode.main()
 
 if __name__ == '__main__': main()
